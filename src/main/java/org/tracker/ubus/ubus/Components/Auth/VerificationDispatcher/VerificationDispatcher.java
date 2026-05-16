fc4828a3568655894a9d57e7a947904a694dc086
Kamogelo-Hlahla <kamogelohlahla18@gmail.com>
2026-05-16 23:10:57 +0200

    buildable server

--- src/main/java/org/tracker/ubus/ubus/Components/Auth/VerificationDispatcher/VerificationDispatcher.java
+++ src/main/java/org/tracker/ubus/ubus/Components/Auth/VerificationDispatcher/VerificationDispatcher.java
@@ -20,7 +20,6 @@ public class VerificationDispatcher {
     public void dispatch(User user) throws OneTimePasswordExistsException {
         switch(user.getRole()) {
             case STUDENT -> sendOtp(user);
-            case DRIVER, STAFF -> sendMagicLink(user);
             default -> throw new IllegalStateException(user.getRole() + " has no Supported Dispatch");
         }
     }
@@ -32,9 +31,4 @@ public class VerificationDispatcher {
         publisher.publish(() -> new OtpEmailVerificationEvent(this, user, internalCarrier));
     }
 
-    private void sendMagicLink(User user) {
-        // i know. ill change it when i get time
-        var internalCarrier = this.oneTimePasswordService.generateOTP(user.getId());
-        publisher.publish(() -> new OtpEmailVerificationEvent(this, user, internalCarrier));
-    }
 }
