c0baa06817cc909768ac1bd0c0a61a7befc0f1ca
LowSoldier20 <masangosimphiwe71@gmail.com>
2026-06-16 21:21:05 +0200

    Added forgot password

--- src/main/java/org/tracker/ubus/ubus/Components/Users/User/Controller/UserController.java
+++ src/main/java/org/tracker/ubus/ubus/Components/Users/User/Controller/UserController.java
@@ -3,6 +3,7 @@ package org.tracker.ubus.ubus.Components.Users.User.Controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.tracker.ubus.ubus.Components.OneTimePassword.DTOs.Internal.OtpInternalCarrier;
 import org.tracker.ubus.ubus.Components.Users.User.DTOs.Requests.EditUserDTO;
 import org.tracker.ubus.ubus.Components.Users.User.DTOs.Responses.UserProfileResponse;
 import org.tracker.ubus.ubus.Components.Users.User.Service.Interface.IUserService;
@@ -24,6 +25,19 @@ class UserController {
     @PutMapping("/edit-profile")
     public UserProfileResponse editProfile(@RequestBody EditUserDTO editUserDTO) {
         return userService.editProfile(editUserDTO);
+    }
+    @PostMapping("/forgot-password")
+    @ResponseStatus(HttpStatus.CONTINUE)
+    public void forgotPassword(@RequestParam String email) {
+        userService.forgotPassword(email);
+    }
+
+    @PutMapping("/valid-forgot-password")
+    @ResponseStatus(HttpStatus.OK)
+    public void validForgotPassword(@RequestParam String email,
+                                    @RequestParam String otp,
+                                    @RequestParam String newPassword) {
+        userService.changePassword(email, otp, newPassword);
     }
 
 }
